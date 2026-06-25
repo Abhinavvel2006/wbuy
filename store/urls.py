ec62d4a2f9b912c937471d83c600bba844160c5b
Abhinavvel2006 <aadilabhinav33@gmail.com>
2026-06-25 17:40:25 +0530

    update url

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,9 @@
-from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('admin/',          admin.site.urls),
     path('',                views.index,        name='index'),
     path('products/',       views.product_list, name='product_list'),
     path('cart/',           views.cart,         name='cart'),
